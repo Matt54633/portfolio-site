43c33b92b429af20ad49e36d0f53d0b5348c17a2
matt54633 <matt54633@gmail.com>
2024-04-21 23:20:58 +0100

    Amend work card

--- src/components/workCard.js
+++ src/components/workCard.js
@@ -9,7 +9,10 @@ const WorkCard = ({ image, title, description, children, href }) => {
             className="aspect-[4/3] rounded-t-xl object-cover"
           />
           <div className="mb-2 flex flex-col gap-2 px-4 py-3.5 text-[1rem]">
-            <h1 className="text-[1.2rem] font-[800]">{title}</h1>
+            <div className="flex flex-row items-center gap-4 justify-between">
+              <h1 className="text-[1.2rem] font-[800]">{title}</h1>
+              <img src="/chevronRight.svg" alt="Right Arrow" className="h-4 w-4 rotate-180" />
+            </div>
             <p className="mb-2 font-medium">{description}</p>
 
             {children}
