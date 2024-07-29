2e6565f17dd4647620e6f2319248162abc34ad9c
matt54633 <matt54633@gmail.com>
2024-07-29 16:09:45 +0100

    Improve Hero Section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,23 +1,27 @@
 const Hero = () => {
   return (
-    <section className="mx-0.5 my-[3rem] md:my-[6rem] lg:my-[7.5rem] flex flex-col w-full">
-      <div className="flex flex-row gap-10 md:mt-0 justify-between items-center">
-        <div className="flex flex-col gap-8 md:gap-8">
-          <img src="/heroImageMedium.png" alt="Hero" className="w-1/2 h-1/2  min-[530px]:w-1/3 md:hidden min-[530px]:mb-[-8rem]  ml-auto mb-[-5rem]" />
-          <div className="flex">
-
-            <h1 className="w-full text-[4.5rem] md:text-[5rem] font-[800] leading-none lg:text-[6rem]">
-              Hey,<br></br>I'm{" "}
-              <span className="content-box bg-spanBG bg-contain bg-bottom bg-no-repeat py-1">Matt</span>
+    <section className="mx-0.5 my-[3rem] flex w-full flex-col md:my-[6rem] lg:my-[7.5rem]">
+      <div className="flex flex-row items-center justify-between gap-4 md:mt-0">
+        <div className="flex flex-col gap-4 md:gap-8">
+          <img
+            src="/heroImageMedium.png"
+            alt="Hero"
+            className="mb-[-6rem] ml-auto hidden h-2/5  w-2/5 min-[340px]:block min-[600px]:hidden"
+          />
+          <div className="flex items-center">
+            <h1 className="w-full text-[4.75rem] font-[800] leading-[5.5rem] md:text-[5rem] md:leading-[6rem] lg:text-[6rem] lg:leading-[6.5rem]">
+              Hey,<br></br>I'm <span className="border-b-4 border-b-primary-blue">Matt</span>
             </h1>
-
           </div>
-          <p className="w-full text-left mb-2 text-[1.8rem] md:text-[2.2rem] leading-[2.5rem] lg:text-[2.5rem]">
+          <p className="mb-2 w-full text-left text-[1rem] font-[500] leading-[2.5rem] min-[350px]:text-[1.25rem]  md:text-[1.75rem] lg:text-[2.2rem]">
             A software engineer from the UK
           </p>
         </div>
-            <img src="/heroImageMedium.png" alt="Hero" className="w-1/4 h-1/4 md:block lg:hidden hidden" />
-        <img src="/heroImage.png" alt="Hero" className="w-1/3 lg:block hidden" />
+        <img
+          src="/heroImage.png"
+          alt="Hero"
+          className="w-2/5 max-[599px]:hidden md:block md:w-2/5 min-[810px]:w-1/3 lg:w-1/3"
+        />
       </div>
       {/* <PrimaryButton buttonName="View my CV" destination={"/cv"} target={"_self"} /> */}
     </section>
