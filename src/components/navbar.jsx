850cd666dccd6d2bc7b9be007237cd1a8a8c63f6
matt54633 <matt54633@gmail.com>
2025-09-14 16:56:32 +0100

    Fix cv file size and navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -17,11 +17,12 @@ const Navbar = () => {
   return (
     <nav 
       style={{ 
-        backdropFilter: isScrolled ? 'blur(12px)' : 'none',
-        WebkitBackdropFilter: isScrolled ? 'blur(12px) contrast(86%)' : 'none',
-        backgroundColor: isScrolled ? '' : 'rgba(44, 44, 44, 0.74)'
+        backdropFilter:  'blur(12px)',
+        WebkitBackdropFilter: 'blur(12px)',
       }} 
-      className="md-py-2  sticky top-4 z-10 m-0 flex justify-between rounded-xl px-3.5 py-2.5 text-[1.3rem] font-bold text-light-blue md:text-[1.2rem]"
+      className={`md-py-2 sticky top-4 z-10 m-0 bg-[#2c2c2cd9] flex justify-between rounded-xl px-3.5 py-2.5 text-[1.3rem] font-bold text-light-blue md:text-[1.2rem] transition-shadow ${
+        isScrolled ? 'shadow-md' : ''
+      }`}
     >
       <h1 className="hover:text-primary-blue transition"><a href="/">Matt Sullivan</a></h1>
       <a href="https://github.com/matt54633/" target="blank" className="flex items-center">
